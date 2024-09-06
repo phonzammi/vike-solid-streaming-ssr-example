aa834921403057fcf297a5535586c952ff7ff992
Muhammad <phonzammizaki@gmail.com>
2024-09-06 09:14:47 +0700

    chore: use `<Head>` instead of `<Config>`

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,31 +1,28 @@
-import { Config } from "vike-solid/Config";
+import { Head } from "vike-solid/Head";
 import { Counter } from "../../components/Counter.jsx";
 import { Movies } from "./Movies.jsx";
 
 export default function Page() {
   return (
     <>
-      <Config
-        Head={(
-          <>
-            <meta name="keywords" content="your, tags" />
-            <meta name="description" content="150 words" />
-            <meta name="subject" content="your website's subject" />
-            <meta name="copyright" content="company name" />
-            <meta name="language" content="ES" />
-            <meta name="robots" content="index,follow" />
-            <meta name="revised" content="Sunday, July 18th, 2010, 5:15 pm" />
-            <meta name="abstract" content="" />
-            <meta name="topic" content="" />
-            <meta name="summary" content="" />
-            <meta name="Classification" content="Business" />
-            <meta name="author" content="name, email@hotmail.com" />
-            <meta name="designer" content="" />
-            <meta name="reply-to" content="email@hotmail.com" />
-            <meta name="owner" content="" />
-          </>
-        )}
-      />
+      <Head>
+        <meta name="keywords" content="your, tags" />
+        <meta name="description" content="150 words" />
+        <meta name="subject" content="your website's subject" />
+        <meta name="copyright" content="company name" />
+        <meta name="language" content="ES" />
+        <meta name="robots" content="index,follow" />
+        <meta name="revised" content="Sunday, July 18th, 2010, 5:15 pm" />
+        <meta name="abstract" content="" />
+        <meta name="topic" content="" />
+        <meta name="summary" content="" />
+        <meta name="Classification" content="Business" />
+        <meta name="author" content="name, email@hotmail.com" />
+        <meta name="designer" content="" />
+        <meta name="reply-to" content="email@hotmail.com" />
+        <meta name="owner" content="" />
+      </Head>
+
       <h1>My Vike app</h1>
       This page is:
       <ul>
